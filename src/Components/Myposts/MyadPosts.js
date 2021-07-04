ee1574e2c84d1572c800429b3975e367707772f4
Naveen Ambookken <naveenambookken@gmail.com>
2021-07-04 23:41:51 +0530

    initial commit

--- src/Components/Myposts/MyadPosts.js
+++ src/Components/Myposts/MyadPosts.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./MyadPosts.css";
 
 function MyadPosts() {
-    return (
-        <div>
-            <h1>My ads</h1>
+  return (
+    <div>
+      <div className="myadsEmptyState">
+        <div className="emptyState">
+          <img src="./Images/emptyStateimg.png" alt=""/>
+          <p className="title">You haven't listed anything yet</p>
+          <p className="subtitle">Let go of what you don't use anymore</p>
+          <div>
+          <button className="emptyStateBtn">start selling</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default MyadPosts
+export default MyadPosts;
